ada7798c46e2fe72efa520b999325c8e84af7450
chickenta2ta <tishizuka1906@icloud.com>
2022-03-18 05:31:35 +0900

    created search form

--- mortgage_loan_search/search/views.py
+++ mortgage_loan_search/search/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.views import generic
 
 
-def index(request):
-    return render(request, "search/index.html")
+class IndexView(generic.ListView):
+    template_name = "search/index.html"
+    context_object_name = "search_result_loans"
+
+    def get_queryset(self):
+        # ここからスタブ
+        search_result_loans = [
+            {
+                "name": "住宅ローン 全期間引下げプラン",
+                "url": "https://www.jibunbank.co.jp/products/homeloan/interest/whole_period/",
+                "interest_rate": 0.389,
+                "borrowing_age": 45,
+                "payoff_age": 80,
+            }
+        ]
+        # ここまでスタブ
+        return search_result_loans
